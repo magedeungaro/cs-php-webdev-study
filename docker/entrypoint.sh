a7d7395cd0bcbc013c648e80afd2f698f6f52d7d
magedeungaro <magedbgt@gmail.com>
2023-07-04 02:00:50 -0300

    wip study

---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -f "vender/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
+
+# php artisan migrate
+# php artisan key:generate
+# php artisan cache:clear
+# php artisan config:clear
+
+# php artisan serve --port=$PORT --host:0.0.0.0 --env=.env
+exec docker-php-entrypoint "$@"
